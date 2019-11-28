24be517c8c7a7aa97ddfdc56903e7a3ef59c0c06
Jorge Luis Ponce Rivera <Blackartorias@hotmail.com>
2019-11-27 20:43:41 -0600

    Update connection.js

--- src/app/config/passport/connection.js
+++ src/app/config/passport/connection.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 // Coloca aquí tus credenciales
 module.exports = mysql.createPool({
-  host: "www.db4free.net",
-  user: "uprintuser",
-  password: "qwertyuiop",
-  database: "uprint"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b0db61a76cd7a3",
+  password: "678596d5",
+  database: "heroku_1a2a8fb0b1eda80"
 });
